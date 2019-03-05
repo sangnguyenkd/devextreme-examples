1ce62c02b6cbfef5219a2fc2a3b3f20539d09831
TruongVietDuc <TruongVietDuc160896>
2019-03-05 17:13:04 +0700

    Adding address

--- datagrid-webapi/datagrid-webapi/Controllers/CustomersController.cs
+++ datagrid-webapi/datagrid-webapi/Controllers/CustomersController.cs
@@ -20,23 +20,7 @@ namespace datagrid_webapi.Controllers
             return Request.CreateResponse(DataSourceLoader.Load(_db.Customers, loadOptions));
         }*/
 
-        [HttpGet]
-        public HttpResponseMessage Get(DataSourceLoadOptions loadOptions)
-        {
-            loadOptions.PrimaryKey = new[] { "CustomerID" };
-
-            var customers = from cm in _db.Customers
-                            select new
-                            {
-                                cm.CustomerID,
-                                cm.ContactName,
-                                cm.City,
-                                OrderQuantity = (from o in _db.Orders
-                                                where o.CustomerID == cm.CustomerID
-                                                select o).Count()
-                         };
-            return Request.CreateResponse(DataSourceLoader.Load(customers, loadOptions));
-        }
+       
       
 
         [HttpPut]
